67d730c53f6fc5307220da7250248560b4c6d185
Alexey Topolyanskiy <devilcoders@protonmail.ch>
2017-04-26 20:07:15 +0200

    Fix build

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-"use strict";
+'use strict'
 
-const webpack = require('webpack');
+const webpack = require('webpack')
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CompressionPlugin = require('compression-webpack-plugin')
 
 const PATHS = {
-  entries: __dirname + '/src/js/',
-  styles: __dirname + '/src/css/',
-  output: __dirname + '/dist/'
+  entries: `${__dirname}/src/js/`,
+  styles: `${__dirname}/src/css/`,
+  output: `${__dirname}/dist/`
 }
 
-let production = process.env.NODE_ENV === 'production';
+let production = process.env.NODE_ENV === 'production'
 
 let config = {
   entry: {
@@ -44,12 +44,8 @@ let config = {
       },
       {
         test: /\.css$/,
-        use: production ? ExtractTextPlugin.extract('css!postcss') : 
-          [
-            'style-loader',
-            'css-loader',
-            'postcss-loader'
-          ]
+        use: production ? ExtractTextPlugin.extract('css-loader!postcss-loader')
+                        : ['style-loader', 'css-loader', 'postcss-loader']
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
@@ -75,12 +71,12 @@ let config = {
       template: './src/slim/index.slim'
     })
   ]
-};
+}
 
 if (production) {
   config.plugins.push(
     new ExtractTextPlugin('css/[name]-[chunkhash].css'),
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.DefinePlugin({
       'process.env': { NODE_ENV: JSON.stringify('production') }
@@ -89,4 +85,4 @@ if (production) {
   )
 }
 
-module.exports = config;
+module.exports = config
