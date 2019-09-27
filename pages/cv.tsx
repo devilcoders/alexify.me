bf1ccfbd2eb0bcf8979a7143abcf04d1bd014e03
Alexey Topolyanskiy <devilcoders@protonmail.ch>
2019-09-27 18:18:08 +0200

    v2 init

---
+++ pages/cv.tsx
@@ -0,0 +1,10 @@
+// --- Dependencies
+import * as React from 'react'
+
+/**
+ * Component
+ */
+
+const CVPage = () => <div><h1>CV</h1></div>
+
+export default CVPage
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+// --- Dependencies
+import * as React from 'react'
+
+/**
+ * Component
+ */
+
+const HomePage = () => <div><h1>Home Page</h1></div>
+
+export default HomePage
